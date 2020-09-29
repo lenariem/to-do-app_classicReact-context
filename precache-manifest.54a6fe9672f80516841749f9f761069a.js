70eee1eb47ce308bb8b219b5f62f49133fe473d8
LenaIT <olena.denysova@gmail.com>
2020-09-29 14:48:15 +0200

    Updates

---
+++ precache-manifest.54a6fe9672f80516841749f9f761069a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cdd729f76a18423f1bd469978c9885",
+    "url": "/lenariem/to-do-app_classicReact-context/index.html"
+  },
+  {
+    "revision": "ae074396ef705f0f2f84",
+    "url": "/lenariem/to-do-app_classicReact-context/static/css/main.5d67a9a3.chunk.css"
+  },
+  {
+    "revision": "937fde3cf0f50ea39794",
+    "url": "/lenariem/to-do-app_classicReact-context/static/js/2.52695d2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lenariem/to-do-app_classicReact-context/static/js/2.52695d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae074396ef705f0f2f84",
+    "url": "/lenariem/to-do-app_classicReact-context/static/js/main.462336bd.chunk.js"
+  },
+  {
+    "revision": "b07a08cd52d135bcd8bd",
+    "url": "/lenariem/to-do-app_classicReact-context/static/js/runtime-main.5ed2d3e0.js"
+  }
+]);
